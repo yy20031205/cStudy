430f12436f1edc86d04a4432b6fc921fd8f35b07
yong <346917395@qq.com>
2022-11-20 13:57:37 +0800

    add c primer plus ch01 example code

---
+++ c_primer_plus/ch01/1-1-helloDog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int dogs;
+
+  printf("How many dogs do you have?\n");
+
+  scanf("%d", &dogs);
+
+  printf("So you have %d dogs!\n", dogs);
+
+  return 0;
+
+
+}
