08ccfb9786638489d5989fefc4a1f92f5dbcf521
yong <346917395@qq.com>
2022-12-09 11:57:33 +0800

    调整目录结构，新增c++PrimerPlus书籍代码

---
+++ 2-c++primerPlus/ch02-开始学习C++/2.1-myfirst.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  // a preprocessor directive
+
+int main()  // function header
+{    // start of function body
+  using namespace std;  
+  cout << "Come up and c++ "; // print message 
+  cout << endl;  // start a new line
+  cout << "You won't regret it!" << endl; 
+}
