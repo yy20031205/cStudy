5755ccea93938f22ccb54d56d71da82554f0c7a3
yong <346917395@qq.com>
2022-12-17 11:13:12 +0800

    添加第三本书第四章代码和笔记

---
+++ 3-Teach Yourself C++/ch04-管理数组和字符串/4.1-arrayDemo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int number [3] = {34,56,-21};
+
+  cout << "first element is: " << number[0] << endl;
+  cout << "last element is: " << number[2] << endl;
+
+  return 0;
+}
